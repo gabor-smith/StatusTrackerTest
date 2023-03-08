a79b87d4d7ef99d81255c5410a2a2365a8dbd57f
gabor-smith <g20kovacs01@gmail.com>
2023-03-08 12:48:45 +0100

    Dropdowns and expand panels to features, table is sortable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-import { MatTableModule } from '@angular/material/table';
 import { Application } from './application';
+import { Sort } from '@angular/material/sort';
 import appsJson from '../assets/versions.json';
 
 @Component({
@@ -12,16 +12,43 @@ import appsJson from '../assets/versions.json';
 export class AppComponent {
   Applications: Application[] = appsJson;
 
-  public displayedColumns: string[] = ['name', 'releasedVersion', 'releaseCandidatesVersions', 'wikiVersion', 'features'];
+  sortedData: Application[];
+
+  public displayedColumns: string[] = ['name', 'releasedVersion', 'releaseCandidatesVersions', 'tagsVersions', 'wikiVersion', 'features', 'compatibilities'];
   public appData = new MatTableDataSource<Application>();
 
   constructor(){
     console.log(this.Applications);
+    this.sortedData = this.Applications.slice();
   }
+
+  sortData(sort: Sort){
+    const data = this.Applications.slice();
+    if(!sort.active || sort.direction === ''){
+      this.sortedData = data;
+      return;
+    }
+
+    this.sortedData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active){
+        case 'name':
+          return compare(a.name, b.name, isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+
+
 
   /*ngOnInit(){
     this.getApplicationsInformation();
   }*/
 
 
+}
+
+function compare(a: number | string, b: number | string, isAsc: boolean){
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,9 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     AppMaterialModule,
     BrowserAnimationsModule,
+    MatButtonModule,
+    MatTableModule,
+    MatExpansionModule,
     HttpClientModule
   ],
   providers: [],
